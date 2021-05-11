f66487b880d106d6fbec3eea8ed311c7dcd7eee2
florent legeron <legeronflo@hotmail.fr>
2021-05-11 15:55:29 +0200

    change method to update route

--- complete.rb
+++ complete.rb
@@ -941,12 +941,8 @@ environment.plugins.prepend('Provide',
 JS
   end
 
-  inject_into_file 'config/routes.rb', after: "root to: 'pages#home'" do
-    <<-'RUBY'
-      match "/404", to: "errors#not_found", via: :all
-      match "/500", to: "errors#internal_server_error", via: :all
-    RUBY
-  end
+  route "match '/404', to: 'errors#not_found', via: :all"
+  route "match '/500', to: 'errors#internal_server_error', via: :all"
 
   generate(:controller, 'errors not_found internal_server_error')
   inject_into_file 'app/controllers/errors_controller.rb', after: 'class ErrorsController < ApplicationController' do
